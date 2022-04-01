c773aa40ccfe382174dd83019388504e152ddf10
cvp1 <54213066+cvp1@users.noreply.github.com>
2022-04-01 09:40:47 -0700

    adding function

--- email.py
+++ email.py
@@ -1,20 +1,23 @@
 
-gm = 0
-hm = 0
-fm_list = []
 
-with open("/Users/craigvandeputte/pyscripts/working/addresses.csv") as add:
-    for x in add:
-        FirstMail = x.strip().split("@")
-        fm_list.append(FirstMail[0]) 
-        if FirstMail[1] == "gmail.com":
-            gm+=1
-        if FirstMail[1] == "hotmail.com":
-            hm+=1 
-print(gm)
-print(hm)
-print(fm_list)
 
+def getmail():
+    gm = 0
+    hm = 0
+    fm_list = []
+    with open("/Users/craigvandeputte/pyscripts/working/addresses.csv") as add:
+        for x in add:
+            FirstMail = x.strip().split("@")
+            fm_list.append(FirstMail[0]) 
+            if FirstMail[1] == "gmail.com":
+                gm+=1
+            if FirstMail[1] == "hotmail.com":
+                hm+=1 
+        print("There are",gm,"gmail addresses")
+        print("There are",hm,"hotmail addresses")
+    return print(fm_list)
+
+getmail()
         
 
 
