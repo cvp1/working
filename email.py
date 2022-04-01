9ec0cfe4f849079a06bbd1906c7b98698f061cee
cvp1 <54213066+cvp1@users.noreply.github.com>
2022-04-01 09:12:28 -0700

    adding email

---
+++ email.py
@@ -0,0 +1,21 @@
+
+gm = 0
+hm = 0
+fm_list = []
+
+with open("/Users/craigvandeputte/pyscripts/addresses.csv") as add:
+    for x in add:
+        FirstMail = x.strip().split("@")
+        fm_list.append(FirstMail[0]) 
+        if FirstMail[1] == "gmail.com":
+            gm+=1
+        if FirstMail[1] == "hotmail.com":
+            hm+=1 
+print(gm)
+print(hm)
+print(fm_list)
+
+        
+
+
+
